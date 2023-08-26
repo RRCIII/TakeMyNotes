d2dd5dc205e91edbbaaa15240ac8e3625187e633
Ricky Carter <randolphcarter5@gmail.com>
2023-08-26 12:05:18 -0400

    created a post route that handles the notes being written to and stored to respected file. Errors added to identify issues

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -12,6 +12,29 @@ const router = express.Router()
 router.use(express.json());
 
 //GET /api/notes 
+router.get('/', (req, res) => {
+  res.json(notesData);
+});
+
+//POST /api/notes
+router.post('/', async (req, res) => {
+  // Create key of 'id', value declared as uuidv4() for the new note object 
+  req.body.id = uuidv4();
+  notesData.push(req.body);
+
+  const dbFilePath = path.join(__dirname, '../db/db.json');
+
+  // WriteFile() to update db.json array; if sucessful send .json w/ status code 201 for "created"
+  try {
+    await fsPromises.wrtieFile(dbFilePath, JSON.stringify(notesData, null, 2))
+    res.status(200).json(req.body);
+    console.log(notesData);
+
+  } catch (error) {
+    console.log("Error writing to mock db.json")
+    res.status(500).json({ error : "Error; Note was not written to db.json"})
+  }
+})
 
 
 
