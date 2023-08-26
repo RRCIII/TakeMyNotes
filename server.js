da861215083029bd7a7bdcaa65cb5a5abefd9efb
Ricky Carter <randolphcarter5@gmail.com>
2023-08-26 10:27:50 -0400

    created sendFiles to respective  paths.

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-
+//Middleware for serving static files 
 app.use(express.static('public'));
 
+//send notes.html for path /notes 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+//send index.html to all paths 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' , 'index.html'))
+});
 
 app.listen(PORT, () => {
     console.log(`Ready to listen to ${PORT}`);
