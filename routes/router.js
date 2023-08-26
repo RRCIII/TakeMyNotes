e706d0105f8501a660744b51f179c3e7a9a23671
Ricky Carter <randolphcarter5@gmail.com>
2023-08-26 09:47:10 -0400

    created server.js, index.js, helper folder to optimize code placement via modularization.

---
+++ routes/router.js
@@ -0,0 +1,33 @@
+const  = require('express').Router();
+
+
+
+let activeNote = { getNotes, saveNote, deleteNote };
+
+const getNotes = () =>
+  fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
+
+const deleteNote = (id) =>
+  fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+
+exports = activeNote
