d116f25af8217db4b4ec21176a02e821a8c9921c
Ricky Carter <randolphcarter5@gmail.com>
2023-08-26 11:35:05 -0400

    deleted index file, added get api route

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -6,9 +6,12 @@ const { v4: uuidv4 } = require('uuid');
 // Mock database for storing notes data
 const notesData = require('../db/db.json');
 
+// Created router
+const router = express.Router()
+// Middleware for parsing incoming JSON data
+router.use(express.json());
 
-
-
+//GET /api/notes 
 
 
 
